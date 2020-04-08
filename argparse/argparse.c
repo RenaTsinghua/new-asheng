1c8d11ce165c602cdb59dd371febd42b66afafec
RenaTsinghua <105906932+RenaTsinghua@users.noreply.github.com>
2020-04-08 15:52:31 +0000

    v0.8.1

--- argparse/argparse.c
+++ argparse/argparse.c
@@ -29,4 +29,7 @@ prefix_cmp(const char *str, const char *prefix)
 
 static void
 argparse_error(struct argparse *self, const struct argparse_option *opt,
-               co
+               const char *reason, int flags)
+{
+    if (flags & OPT_LONG) {
+        fprintf(stderr, "error:
