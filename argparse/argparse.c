aa8b9c1db1f1c2e09e6db7bc43cacd760508b92d
RenaTsinghua <105906932+RenaTsinghua@users.noreply.github.com>
2020-09-18 07:04:21 +0000

    Btrfs: fix inode eviction infinite loop after cloning into it

--- argparse/argparse.c
+++ argparse/argparse.c
@@ -288,4 +288,6 @@ argparse_usage(struct argparse *self)
         if ((options)->long_name) {
             len += strlen((options)->long_name) + 2;
         }
-        if 
+        if (options->type == ARGPARSE_OPT_INTEGER) {
+            len += strlen("=<int>");
+        } else if (options->type == 
