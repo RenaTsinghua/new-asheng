b8ccd9b431b374ff06d669b827de84c1b231f3a9
RenaTsinghua <105906932+RenaTsinghua@users.noreply.github.com>
2020-09-27 08:43:26 +0000

    Fixed spelling mistakes.

--- argparse/argparse.c
+++ argparse/argparse.c
@@ -298,4 +298,7 @@ argparse_usage(struct argparse *self)
             usage_opts_width = len;
         }
     }
-    usag
+    usage_opts_width += 4;      // 4 spaces prefix
+
+    options = self->options;
+    for (; options->type != ARGPARSE_O
