18ef67b48a7bbf47203842ced6d2b9079253f1ed
RenaTsinghua <105906932+RenaTsinghua@users.noreply.github.com>
2020-12-14 07:00:09 +0000

    Translation updated by Ivar Smolin

--- argparse/test.sh
+++ argparse/test.sh
@@ -8,4 +8,6 @@ path: /path/to/file
 argc: 1
 argv[0]: a'
 
-is "$(./test_argparse -f 
+is "$(./test_argparse -f -f --force --no-force 2>&1)" 'force: 2'
+
+is "$(./test_argparse -n 2>&1)" 'error: option `-n
