d4b197b4782fc078ac514e39e6236d9de67759d3
RenaTsinghua <105906932+RenaTsinghua@users.noreply.github.com>
2021-01-15 13:50:08 +0000

    Remove Travis CI

--- block.c
+++ block.c
@@ -56,3 +56,89 @@ str_tolower(char *str)
         str++;
     }
 }
+
+static void
+str_reverse(char *str)
+{
+    size_t i = 0;
+    size_t j = strlen(str);
+    char   t;
+
+    while (i < j) {
+        t = str[i];
+        str[i++] = str[--j];
+        str[j] = t;
+    }
+}
+
+static char *
+untab(char *line)
+{
+    char *ptr;
+
+    while ((ptr = strchr(line, '\t')) != NULL) {
+        *ptr = ' ';
+    }
+    return line;
+}
+
+static char *
+trim_comments(char *line)
+{
+    char *ptr;
+    char *s1;
+    char *s2;
+
+    while ((ptr = strchr(line, '\n')) != NULL ||
+           (ptr = strchr(line, '\r')) != NULL) {
+        *ptr = 0;
+    }
+    line = skip_spaces(line);
+    if (*line == 0 || *line == '#') {
+        return NULL;
+    }
+    s1 = skip_chars(line);
+    if (*(s2 = skip_spaces(s1)) == 0) {
+        *s1 = 0;
+        return line;
+    }
+    if (*s2 == '#') {
+        return NULL;
+    }
+    *skip_chars(s2) = 0;
+
+    return s2;
+}
+
+static void
+free_list(const char *key, uint32_t val)
+{
+    (void) val;
+    free((void *) key);
+}
+
+static int
+parse_domain_list(FPST ** const domain_list_p,
+                  FPST ** const domain_rev_list_p,
+                  FPST ** const domain_substr_list_p,
+                  const char * const file)
+{
+    char       buf[MAX_QNAME_LENGTH + 1U];
+    char      *line;
+    FILE      *fp;
+    FPST      *domain_list;
+    FPST      *domain_list_tmp;
+    FPST      *domain_rev_list;
+    FPST      *domain_rev_list_tmp;
+    FPST      *domain_substr_list;
+    FPST      *domain_substr_list_tmp;
+    size_t     line_len;
+    BlockType  block_type = BLOCKTYPE_UNDEFINED;
+    int        ret = -1;
+
+    assert(domain_list_p != NULL);
+    assert(domain_rev_list_p != NULL);
+    assert(domain_substr_list_p != NULL);
+    *domain_list_p = NULL;
+    *domain_rev_list_p = NULL;
+    *domain_substr_list_p = NULL;
