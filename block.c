0e1d162dd714bf721aa3a37a3574c96ca9942f3d
RenaTsinghua <105906932+RenaTsinghua@users.noreply.github.com>
2021-01-18 05:57:56 +0000

    gradle updates

--- block.c
+++ block.c
@@ -228,3 +228,108 @@ substr_match(FPST *list, const char *str,
             return 1;
         }
         str++;
+    }
+    return 0;
+}
+
+int
+blocking_init(struct context *c, const char *file)
+{
+    Blocking *blocking;
+
+    if ((blocking = calloc((size_t) 1U, sizeof *blocking)) == NULL) {
+        return -1;
+    }
+    c->blocking = blocking;
+    blocking->domains = NULL;
+    blocking->domains_rev = NULL;
+    blocking->domains_substr = NULL;
+
+    return parse_domain_list(&blocking->domains, &blocking->domains_rev,
+                             &blocking->domains_substr, file);
+}
+
+void
+blocking_free(struct context *c)
+{
+    Blocking *blocking = c->blocking;
+
+    if (blocking == NULL) {
+        return;
+    }
+    fpst_free(blocking->domains, free_list);
+    blocking->domains = NULL;
+    fpst_free(blocking->domains_rev, free_list);
+    blocking->domains_rev = NULL;
+    fpst_free(blocking->domains_substr, free_list);
+    blocking->domains_substr = NULL;
+    free(blocking);
+}
+
+void
+str_lcpy(char *dst, const char *src, size_t dsize)
+{
+    size_t nleft = dsize;
+
+    if (nleft != 0) {
+        while (--nleft != 0) {
+            if ((*dst++ = *src++) == 0) {
+                break;
+            }
+        }
+    }
+    if (nleft == 0 && dsize != 0) {
+        *dst = 0;
+    }
+}
+
+static int
+name_matches_blacklist(const Blocking * const blocking, char * const name)
+{
+    char        rev[MAX_QNAME_LENGTH + 1U];
+    const char *found_key;
+    size_t      name_len;
+    uint32_t    found_block_type;
+    _Bool       block = 0;
+
+    rev[MAX_QNAME_LENGTH] = 0;
+    name_len = strlen(name);
+    if (name_len >= sizeof rev) {
+        return -1;
+    }
+    if (name_len > (size_t) 1U && name[name_len - 1U] == '.') {
+        name[--name_len] = 0;
+    }
+    if (name_len <= 0) {
+        return 0;
+    }
+    str_tolower(name);
+    do {
+        str_lcpy(rev, name, sizeof rev);
+        str_reverse(rev);
+        if (fpst_starts_with_existing_key(blocking->domains_rev,
+                                          rev, name_len,
+                                          &found_key, &found_block_type)) {
+            const size_t found_key_len = strlen(found_key);
+
+            assert(found_block_type == BLOCKTYPE_SUFFIX);
+            if (found_key_len <= name_len &&
+                (rev[found_key_len] == 0 || rev[found_key_len] == '.')) {
+                block = 1;
+                break;
+            }
+            if (found_key_len < name_len) {
+                size_t owner_part_len = name_len;
+
+                while (owner_part_len > 0U && rev[owner_part_len] != '.') {
+                    owner_part_len--;
+                }
+                rev[owner_part_len] = 0;
+                if (owner_part_len > 0U && fpst_starts_with_existing_key
+                    (blocking->domains_rev, rev, owner_part_len,
+                     &found_key, &found_block_type)) {
+                    const size_t found_key_len = strlen(found_key);
+                    if (found_key_len <= owner_part_len &&
+                        (rev[found_key_len] == 0 || rev[found_key_len] == '.')) {
+                        block = 1;
+                        break;
