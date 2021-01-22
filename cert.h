2086874d5279f09faa16b85bed995180e94484fe
RenaTsinghua <105906932+RenaTsinghua@users.noreply.github.com>
2021-01-22 21:19:03 +0000

    ChangeLog fix.

--- cert.h
+++ cert.h
@@ -5,4 +5,10 @@
 #define CERT_MAGIC_CERT "DNSC"
 #define CERT_MAJOR_VERSION 1
 #define CERT_MINOR_VERSION 0
-#define CERT_OLD_MAGIC_HEAD
+#define CERT_OLD_MAGIC_HEADER "7PYqwfzt"
+
+#define CERT_FILE_EXPIRE_DAYS 1
+
+struct SignedCert {
+    uint8_t magic_cert[4];
+    uint8
