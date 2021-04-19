1918f29c362198aafc6281d7b519c2d0cfa77958
RenaTsinghua <105906932+RenaTsinghua@users.noreply.github.com>
2021-04-19 19:31:30 +0000

    Bump to 0.2.6

--- debug.h
+++ debug.h
@@ -9,4 +9,7 @@
 #define debug_assert(e) ((e) ? (void)0 : (_debug_assert(#e, __FILE__, __LINE__), _exit(1)))
 void _debug_assert(char *err, char *file, int line);
 
-void debug_init
+void debug_init(void);
+void debug_segv_handler(int sig, siginfo_t *info, void *secret);
+
+#endif
