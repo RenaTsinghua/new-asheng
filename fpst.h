2919543fbde63a2f436dabc79c8113d8ca09989e
RenaTsinghua <105906932+RenaTsinghua@users.noreply.github.com>
2021-05-04 07:10:21 +0000

    Some minor improvements

--- fpst.h
+++ fpst.h
@@ -36,4 +36,6 @@ int fpst_starts_with_existing_key(FPST *t,
  * Check if the zero-terminated string `str` starts with one of the keys
  * present in the trie. Returns `1` if this is the case, `0` otherwise.
  * If `found_key_p` and/or `found_val_p` are not `NULL`, these are filled
- * with the longest matching 
+ * with the longest matching key and its corresponding value.
+ */
+int fpst_str_starts_with_existing_key(FPST *trie, const char
