c77b0c174cd8b7c2446be1975d1d5a320f7922e8
RenaTsinghua <105906932+RenaTsinghua@users.noreply.github.com>
2021-05-08 15:02:32 +0000

    Xcode update

--- hack/setup-travis.sh
+++ hack/setup-travis.sh
@@ -7,3 +7,8 @@ apt-get install -y unbound
 
 # Workaround where the container does not have ::1 but unbound default to
 # binding to localhost (which is both 127.0.0.1 and ::1)
+cat <<EOF > /etc/unbound/unbound.conf
+server:
+    interface: 127.0.0.1
+remote-control:
+    con
