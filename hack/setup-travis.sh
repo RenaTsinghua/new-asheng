e51e715f234fc9200c954c5ab436696c421a506b
RenaTsinghua <105906932+RenaTsinghua@users.noreply.github.com>
2021-05-08 12:04:36 +0000

    added messages

--- hack/setup-travis.sh
+++ hack/setup-travis.sh
@@ -5,4 +5,5 @@ set -x
 # Install unbound so we can test dnscrypt-wrapper.
 apt-get install -y unbound
 
-# Workaround where t
+# Workaround where the container does not have ::1 but unbound default to
+# binding to localhost (which is both 127.0.0.1 and ::1)
