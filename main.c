ed37a873595f837e22c02862fad1438caee050c0
RenaTsinghua <105906932+RenaTsinghua@users.noreply.github.com>
2021-06-07 18:34:00 +0000

    Upgrading CodeIgniter.

--- main.c
+++ main.c
@@ -232,4 +232,9 @@ filter_signed_certs(struct context *c)
                 filtered_certs[j].version_minor[1] == c->signed_certs[i].version_minor[1]) {
                 found = 1;
                 if (ntohl(*(uint32_t *)filtered_certs[j].serial) < ntohl(*(uint32_t *)c->signed_certs[i].serial)) {
-                    filtered_certs[j] = c->signed_
+                    filtered_certs[j] = c->signed_certs[i];
+                }
+            }
+        }
+        if (found == 0) {
+            filtere
