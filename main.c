0881241c1e8d1a70b4491077bc4a838b5970d0f1
RenaTsinghua <105906932+RenaTsinghua@users.noreply.github.com>
2021-09-10 06:51:01 +0000

    added french translation

--- main.c
+++ main.c
@@ -499,4 +499,5 @@ main(int argc, const char **argv)
         OPT_BOOLEAN('V', "verbose", &verbose,
                     "show verbose logs (specify more -VVV to increase verbosity)"),
         OPT_BOOLEAN('v', "version", NULL, "show version info", show_version_cb),
-#ifdef HAVE_CRYPTO_BOX_CURVE25519XCHACHA20POLY1305_
+#ifdef HAVE_CRYPTO_BOX_CURVE25519XCHACHA20POLY1305_OPEN_EASY
+        OPT_BOOLEAN('x', "xchacha20", &use_xchacha20, "generate a certificate for use with the xc
