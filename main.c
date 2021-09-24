0ccf877f5d1194c00d38678b4d9107b20c2ff929
RenaTsinghua <105906932+RenaTsinghua@users.noreply.github.com>
2021-09-24 07:06:14 +0000

    3.3.6

--- main.c
+++ main.c
@@ -540,4 +540,8 @@ main(int argc, const char **argv)
     c.keypairs = NULL;
 
     if (gen_provider_keypair) {
-        uint8_t provider_publickey[crypto_sign_ed25519_PUBLICKEYBYT
+        uint8_t provider_publickey[crypto_sign_ed25519_PUBLICKEYBYTES];
+        uint8_t provider_secretkey[crypto_sign_ed25519_SECRETKEYBYTES];
+
+        if (!dnssec) {
+           
