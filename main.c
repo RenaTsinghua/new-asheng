63996d0e146afdce18bc26518f0fc6704dde04ac
RenaTsinghua <105906932+RenaTsinghua@users.noreply.github.com>
2022-02-14 11:38:34 +0000

    Fix travis.

--- main.c
+++ main.c
@@ -642,4 +642,7 @@ main(int argc, const char **argv)
     }
 
     if (gen_crypt_keypair) {
-        printf("Generate crypt key pai
+        printf("Generate crypt key pair...");
+        fflush(stdout);
+        if ((c.keypairs = sodium_malloc(sizeof *c.keypairs)) == NULL)
+            e
