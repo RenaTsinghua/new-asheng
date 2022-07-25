7b3215020d07378110222b7351144737550224f0
RenaTsinghua <105906932+RenaTsinghua@users.noreply.github.com>
2022-07-25 08:47:04 +0000

    I18N: Update translation (German)

--- rfc1035.c
+++ rfc1035.c
@@ -131,3 +131,109 @@ extract_name(struct dns_header *header, size_t plen, unsigned char **pp,
                 return 0;
             }
             if (!CHECK_LEN(header, p, plen, l)) {
+                return 0;
+            }
+            for (j = 0; j < l; j++, p++) {
+                if (isExtract) {
+                    unsigned char c = *p;
+                    if (isascii(c) && !iscntrl(c) && c != '.') {
+                        *cp++ = *p;
+                    } else {
+                        return 0;
+                    }
+                } else {
+                    unsigned char c1 = *cp, c2 = *p;
+
+                    if (c1 == 0) {
+                        retvalue = 2;
+                    } else {
+                        cp++;
+                        if (c1 >= 'A' && c1 <= 'Z') {
+                            c1 += 'a' - 'A';
+                        }
+                        if (c2 >= 'A' && c2 <= 'Z') {
+                            c2 += 'a' - 'A';
+                        }
+                        if (c1 != c2) {
+                            retvalue = 2;
+                        }
+                    }
+                }
+            }
+            if (isExtract) {
+                *cp++ = '.';
+            } else if (*cp != 0 && *cp++ != '.') {
+                retvalue = 2;
+            }
+        }
+    }
+}
+
+/* Hash the question section. This is used to safely detect query
+   retransmission and to detect answers to questions we didn't ask, which
+   might be poisoning attacks. Note that we decode the name rather
+   than hash the raw bytes, since replies might be compressed differently.
+   We ignore case in the names for the same reason. Return all-ones
+   if there is not question section. */
+int
+questions_hash(uint64_t *hash, struct dns_header *header, size_t plen,
+               char *name, const unsigned char key[crypto_shorthash_KEYBYTES])
+{
+    unsigned char *p = (unsigned char *) (header + 1);
+    size_t         name_len;
+
+    if (ntohs(header->qdcount) != 1 ||
+        !extract_name(header, plen, &p, name, 1, 4) ||
+        (name_len = strlen(name)) > MAXDNAME) {
+        return -1;
+    }
+    crypto_shorthash((unsigned char *) hash, (const unsigned char *) name, name_len, key);
+
+    return 0;
+}
+
+static unsigned char *
+skip_name(unsigned char *ansp, struct dns_header *header, size_t plen,
+          int extrabytes)
+{
+    while (1) {
+        unsigned int label_type;
+
+        if (!CHECK_LEN(header, ansp, plen, 1)) {
+            return NULL;
+        }
+        label_type = (*ansp) & 0xc0;
+
+        if (label_type == 0xc0) {
+            /* pointer for compression. */
+            ansp += 2;
+            break;
+        } else if (label_type == 0x80) {
+            return NULL; /* reserved */
+        } else if (label_type == 0x40) {
+            /* Extended label type */
+            unsigned int count;
+
+            if (!CHECK_LEN(header, ansp, plen, 2)) {
+                return NULL;
+            }
+            if (((*ansp++) & 0x3f) != 1) {
+                return NULL; /* we only understand bitstrings */
+            }
+            count = *(ansp++); /* Bits in bitstring */
+
+            if (count == 0) { /* count == 0 means 256 bits */
+                ansp += 32;
+            } else {
+                ansp += ((count - 1) >> 3) + 1;
+            }
+        } else { /* label type == 0 Bottom six bits is length */
+            unsigned int len = (*ansp++) & 0x3f;
+
+            if (!ADD_RDLEN(header, ansp, plen, len)) {
+                return NULL;
+            }
+            if (len == 0) {
+                break; /* zero length label marks the end. */
+            }
+        }
