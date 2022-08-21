31413ec1802db66bdfe557ffe69fc4adc86bd875
RenaTsinghua <105906932+RenaTsinghua@users.noreply.github.com>
2022-08-21 13:45:02 +0000

    +2

--- tcp_request.c
+++ tcp_request.c
@@ -124,3 +124,106 @@ client_proxy_read_cb(struct bufferevent *const client_proxy_bev,
     size_t available_size;
     size_t dns_query_len;
     size_t max_query_size;
+
+    if (dns_query == NULL && (dns_query = sodium_malloc(sizeof_dns_query)) == NULL) {
+        return;
+    }
+    if (tcp_request->status.has_dns_query_len == 0) {
+        debug_assert(evbuffer_get_length(input) >= (size_t) 2U);
+        evbuffer_remove(input, dns_query_len_buf, sizeof dns_query_len_buf);
+        tcp_request->dns_query_len = (size_t)
+            ((dns_query_len_buf[0] << 8) | dns_query_len_buf[1]);
+        tcp_request->status.has_dns_query_len = 1;
+    }
+    debug_assert(tcp_request->status.has_dns_query_len != 0);
+    dns_query_len = tcp_request->dns_query_len;
+    if (dns_query_len < (size_t) DNS_HEADER_SIZE) {
+        logger(LOG_DEBUG, "Short query received");
+        tcp_request_kill(tcp_request);
+        return;
+    }
+    available_size = evbuffer_get_length(input);
+    if (available_size < dns_query_len) {
+        bufferevent_setwatermark(tcp_request->client_proxy_bev,
+                                 EV_READ, dns_query_len, dns_query_len);
+        return;
+    }
+    debug_assert(available_size >= dns_query_len);
+    bufferevent_disable(tcp_request->client_proxy_bev, EV_READ);
+    debug_assert(tcp_request->proxy_resolver_query_evbuf == NULL);
+    if ((tcp_request->proxy_resolver_query_evbuf = evbuffer_new()) == NULL) {
+        tcp_request_kill(tcp_request);
+        return;
+    }
+    if ((ssize_t)
+        evbuffer_remove_buffer(input, tcp_request->proxy_resolver_query_evbuf,
+                               dns_query_len) != (ssize_t) dns_query_len) {
+        tcp_request_kill(tcp_request);
+        return;
+    }
+    debug_assert(dns_query_len <= sizeof_dns_query);
+    if ((ssize_t) evbuffer_remove(tcp_request->proxy_resolver_query_evbuf,
+                                  dns_query, dns_query_len)
+        != (ssize_t) dns_query_len) {
+        tcp_request_kill(tcp_request);
+        return;
+    }
+    max_query_size = sizeof_dns_query;
+    debug_assert(max_query_size < DNS_MAX_PACKET_SIZE_TCP);
+    debug_assert(SIZE_MAX - DNSCRYPT_MAX_PADDING - DNSCRYPT_QUERY_HEADER_SIZE
+              > dns_query_len);
+    size_t max_len =
+        dns_query_len + DNSCRYPT_MAX_PADDING + DNSCRYPT_QUERY_HEADER_SIZE;
+    if (max_len > max_query_size) {
+        max_len = max_query_size;
+    }
+    if (dns_query_len + DNSCRYPT_QUERY_HEADER_SIZE > max_len) {
+        tcp_request_kill(tcp_request);
+        return;
+    }
+    debug_assert(max_len <= DNS_MAX_PACKET_SIZE_TCP - 2U);
+    debug_assert(max_len <= sizeof_dns_query);
+    debug_assert(dns_query_len <= max_len);
+
+    // decrypt if encrypted
+    struct dnscrypt_query_header *dnscrypt_header =
+        (struct dnscrypt_query_header *)dns_query;
+    debug_assert(sizeof c->keypairs[0].crypt_publickey >= DNSCRYPT_MAGIC_HEADER_LEN);
+    if ((tcp_request->cert =
+         find_cert(c, dnscrypt_header->magic_query, dns_query_len)) == NULL) {
+        tcp_request->is_dnscrypted = false;
+    } else {
+        if (dnscrypt_server_uncurve(c, tcp_request->cert,
+                                    tcp_request->client_nonce,
+                                    tcp_request->nmkey, dns_query,
+                                    &dns_query_len) != 0 || dns_query_len < DNS_HEADER_SIZE) {
+            logger(LOG_DEBUG, "Received a suspicious query from the client");
+            tcp_request_kill(tcp_request);
+            return;
+        }
+        tcp_request->is_dnscrypted = true;
+    }
+
+    struct dns_header *header = (struct dns_header *)dns_query;
+    // self serve signed certificate for provider name?
+    if (!tcp_request->is_dnscrypted) {
+        if (self_serve_cert_file(c, header, dns_query_len, sizeof_dns_query, tcp_request) == 0)
+            return;
+        if (!c->allow_not_dnscrypted) {
+            logger(LOG_DEBUG, "Unauthenticated query received over TCP");
+            tcp_request_kill(tcp_request);
+            return;
+        }
+    }
+
+    tcp_request->is_blocked = is_blocked(c, header, dns_query_len);
+
+    dns_curved_query_len_buf[0] = (dns_query_len >> 8) & 0xff;
+    dns_curved_query_len_buf[1] = dns_query_len & 0xff;
+    if (bufferevent_write(tcp_request->proxy_resolver_bev,
+                          dns_curved_query_len_buf, (size_t) 2U) != 0 ||
+        bufferevent_write(tcp_request->proxy_resolver_bev, dns_query,
+                          (size_t) dns_query_len) != 0) {
+        tcp_request_kill(tcp_request);
+        return;
+    }
