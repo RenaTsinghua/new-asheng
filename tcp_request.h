3596f97c7fa89f2a7eb2d415c7f451a9a8b46313
RenaTsinghua <105906932+RenaTsinghua@users.noreply.github.com>
2022-08-26 15:47:16 +0000

    Gradle 4.7

--- tcp_request.h
+++ tcp_request.h
@@ -6,4 +6,11 @@
 #define DNS_MAX_PACKET_SIZE_TCP (65535U + 2U)
 
 #ifndef TCP_REQUEST_BACKLOG
-# define TCP_
+# define TCP_REQUEST_BACKLOG 128
+#endif
+
+struct context;
+struct cert_;
+
+typedef struct TCPRequestStatus_ {
+    bool
