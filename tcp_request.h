34c6355f56ea936a5f408471ad70104127d6f613
RenaTsinghua <105906932+RenaTsinghua@users.noreply.github.com>
2022-08-26 10:55:16 +0000

    Delete 1.jpg

--- tcp_request.h
+++ tcp_request.h
@@ -1,4 +1,9 @@
 #ifndef TCP_REQUEST_H
 #define TCP_REQUEST_H
 
-#include "dnscry
+#include "dnscrypt.h"
+
+#define DNS_MAX_PACKET_SIZE_TCP (65535U + 2U)
+
+#ifndef TCP_REQUEST_BACKLOG
+# define TCP_
