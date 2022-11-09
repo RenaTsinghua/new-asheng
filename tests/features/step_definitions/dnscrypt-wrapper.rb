b881866c21dbad7471dc68246a5efb4ab8573747
RenaTsinghua <105906932+RenaTsinghua@users.noreply.github.com>
2022-11-09 20:14:37 +0000

    Updated the pandoc feedstock.

--- tests/features/step_definitions/dnscrypt-wrapper.rb
+++ tests/features/step_definitions/dnscrypt-wrapper.rb
@@ -5,4 +5,8 @@ WRAPPER_IP = '127.0.0.1'
 WRAPPER_PORT = 5443
 
 Before do
-  @resolver = Net::DNS:
+  @resolver = Net::DNS::Resolver.new(nameserver: WRAPPER_IP, port: WRAPPER_PORT)
+end
+
+After do
+  Process.kill("K
