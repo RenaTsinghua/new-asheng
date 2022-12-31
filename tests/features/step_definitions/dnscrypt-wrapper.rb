14d1a590294b4e9fe592f65af43eab215ab20935
RenaTsinghua <105906932+RenaTsinghua@users.noreply.github.com>
2022-12-31 22:08:03 +0000

    Fixed javadocs

--- tests/features/step_definitions/dnscrypt-wrapper.rb
+++ tests/features/step_definitions/dnscrypt-wrapper.rb
@@ -64,3 +64,6 @@ Then /^dnscrypt\-wrapper does not return "([^"]*)"$/ do |certfile|
   expect(@answer_section.collect { |a| a.txt.strip().force_encoding('UTF-8') }).not_to include(cert)
 end
 
+Then /^a "(.*)" is thrown$/ do |error|
+  @error.class.to_s == error
+end
