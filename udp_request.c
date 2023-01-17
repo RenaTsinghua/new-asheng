7bdef4ae50ee75d3f524e6e6b2ac6223eb5a79a8
RenaTsinghua <105906932+RenaTsinghua@users.noreply.github.com>
2023-01-17 08:39:26 +0000

    lua

--- udp_request.c
+++ udp_request.c
@@ -158,3 +158,93 @@ sendto_with_retry(SendtoWithRetryCtx *const ctx)
 
     if (sendto(ctx->handle, ctx->buffer, ctx->length, ctx->flags,
                ctx->dest_addr, ctx->dest_len) == (ssize_t) ctx->length) {
+        cb = ctx->cb;
+        if (udp_request->sendto_retry_timer != NULL) {
+            ctx_cb = event_get_callback_arg(udp_request->sendto_retry_timer);
+            assert(ctx_cb != NULL);
+            assert(ctx_cb->udp_request == ctx->udp_request);
+            assert(ctx_cb->buffer == ctx->buffer);
+            free(ctx_cb);
+            event_free(udp_request->sendto_retry_timer);
+            udp_request->sendto_retry_timer = NULL;
+        }
+        if (cb) {
+            cb(udp_request);
+        }
+        return 0;
+    }
+
+    err = evutil_socket_geterror(udp_request->client_proxy_handle);
+    logger(LOG_WARNING, "sendto: [%s]", evutil_socket_error_to_string(err));
+
+    retriable = (err == ENOBUFS || err == ENOMEM ||
+                 err == EAGAIN || err == EINTR);
+
+    if (retriable == 0) {
+        udp_request_kill(udp_request);
+        return -1;
+    }
+    assert(DNS_QUERY_TIMEOUT < UCHAR_MAX);
+    if (++(udp_request->retries) > DNS_QUERY_TIMEOUT) {
+        udp_request_kill(udp_request);
+        return -1;
+    }
+    if (udp_request->sendto_retry_timer != NULL) {
+        ctx_cb = event_get_callback_arg(udp_request->sendto_retry_timer);
+        assert(ctx_cb != NULL);
+        assert(ctx_cb->udp_request == ctx->udp_request);
+        assert(ctx_cb->buffer == ctx->buffer);
+    } else {
+        if ((ctx_cb = malloc(sizeof *ctx_cb)) == NULL) {
+            udp_request_kill(udp_request);
+            return -1;
+        }
+        assert(ctx_cb ==
+               event_get_callback_arg(udp_request->sendto_retry_timer));
+        *ctx_cb = *ctx;
+        if ((udp_request->sendto_retry_timer =
+             evtimer_new(udp_request->context->event_loop,
+                         sendto_with_retry_timer_cb, ctx_cb)) == NULL) {
+            free(ctx_cb);
+            udp_request_kill(udp_request);
+            return -1;
+        }
+    }
+    const struct timeval tv = {
+        .tv_sec = (time_t) UDP_DELAY_BETWEEN_RETRIES,.tv_usec = 0
+    };
+    evtimer_add(udp_request->sendto_retry_timer, &tv);
+    return -1;
+
+}
+
+static void
+timeout_timer_cb(evutil_socket_t timeout_timer_handle, short ev_flags,
+                 void *const udp_request_)
+{
+    UDPRequest *const udp_request = udp_request_;
+
+    (void)ev_flags;
+    (void)timeout_timer_handle;
+    logger(LOG_DEBUG, "resolver timeout (UDP)");
+    udp_request_kill(udp_request);
+}
+
+/**
+ * Return 0 if served.
+ */
+static int
+self_serve_cert_file(struct context *c, struct dns_header *header,
+                     size_t dns_query_len, size_t max_len, UDPRequest *udp_request)
+{
+    int ret = dnscrypt_self_serve_cert_file(c, header, &dns_query_len, max_len);
+    if (ret == 0) {
+        SendtoWithRetryCtx retry_ctx = {
+            .udp_request = udp_request,
+            .handle = udp_request->client_proxy_handle,
+            .buffer = header,
+            .length = dns_query_len,
+            .flags = 0,
+            .dest_addr = (struct sockaddr *)&udp_request->client_sockaddr,
+            .dest_len = udp_request->client_sockaddr_len,
+            .cb = udp_request_kill
