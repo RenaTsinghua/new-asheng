d9eb716f176f728edda0b8b0d41da687aa467352
RenaTsinghua <105906932+RenaTsinghua@users.noreply.github.com>
2023-02-26 20:56:13 +0000

    3.2.13

--- udp_request.h
+++ udp_request.h
@@ -37,4 +37,7 @@ typedef RB_HEAD(UDPRequestQueue_, UDPRequest_) UDPRequestQueue;
 
 int udp_listener_bind(struct context *c);
 int udp_listener_start(struct context *c);
-void udp_listener_stop(struct context 
+void udp_listener_stop(struct context *c);
+int udp_listener_kill_oldest_request(struct context *c);
+
+#endif
